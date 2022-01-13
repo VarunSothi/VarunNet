9f2921fcb58a052a929591d60a4dea0a58564fa2
Varun Sothilingam <varunsothilingam@varuns-mbp.home>
2022-01-13 16:00:35 +0000

    Added a go to top button

--- scripts/main.js
+++ scripts/main.js
@@ -13,8 +13,8 @@ myImage.onclick = function() {
 
 // Welcome message
 
-let myButton = document.querySelector('button');
-let myHeading = document.querySelector('h1');
+/*let myButton = document.querySelector('button');
+let myHeading = document.querySelector('h1');*/
 
 function setUserName() {
   let myName = prompt('Please enter your name.');
@@ -33,6 +33,29 @@ function setUserName() {
   myHeading.innerHTML = 'Mozilla is cool, ' + storedName;
 }*/
 
-myButton.onclick = function() {
+/*myButton.onclick = function() {
   setUserName();
+}*/
+
+
+
+
+//Get the button:
+mybutton = document.getElementById("myBtn");
+
+// When the user scrolls down 20px from the top of the document, show the button
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+    mybutton.style.display = "block";
+  } else {
+    mybutton.style.display = "none";
+  }
 }
+
+// When the user clicks on the button, scroll to the top of the document
+function topFunction() {
+  document.body.scrollTop = 0; // For Safari
+  document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
+} 
